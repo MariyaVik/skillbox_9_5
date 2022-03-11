83990c627459f629935d4be3dd14db8a7e197756
Mariya Pelageicheva <pelagei4eva@gmail.com>
2022-03-11 22:23:22 +0500

    first commit

---
+++ lib/view_hotels.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ViewHotels extends StatefulWidget {
+  ViewHotels({Key? key}) : super(key: key);
+
+  @override
+  State<ViewHotels> createState() => _ViewHotelsState();
+}
+
+class _ViewHotelsState extends State<ViewHotels> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
