9c7aab794d3e2710a84fd42ad3011be5d63a7742
Mariya Pelageicheva <pelagei4eva@gmail.com>
2022-03-12 13:54:48 +0500

    switch of view done

--- lib/widgets/hotels_list_widget.dart
+++ lib/widgets/hotels_list_widget.dart
@@ -8,8 +8,47 @@ class HotelsWidget extends StatefulWidget {
 }
 
 class _HotelsWidgetState extends State<HotelsWidget> {
+  bool _isGrid = false;
+  final List<String> _fakeData = ['1', '2'];
+
+  void _onTabList() {
+    if (_isGrid) {
+      _isGrid = false;
+      setState(() {});
+    }
+  }
+
+  void _onTabGrid() {
+    if (!_isGrid) {
+      _isGrid = true;
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(onPressed: _onTabList, icon: Icon(Icons.list)),
+            IconButton(onPressed: _onTabGrid, icon: Icon(Icons.apps)),
+          ],
+        ),
+        body: _isGrid
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemCount: _fakeData.length,
+                itemBuilder: (context, index) {
+                  return Card(child: Text(_fakeData[index]));
+                })
+            : ListView.builder(
+                itemCount: _fakeData.length,
+                itemBuilder: (context, index) {
+                  return Card(child: Text(_fakeData[index]));
+                }),
+      ),
+    );
   }
 }
